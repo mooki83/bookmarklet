30f57907a46ca3d80aad1de4902004fec346704f
mooki <mooki83@gmail.com>
2016-08-22 19:04:34 +0900

    네이버 블로그 클리핑을 위한 JS 추가

---
+++ naverblog_for_evernote.js
@@ -0,0 +1,31 @@
+var chkRun = false;
+
+function goBlog(id, no) {
+	location.href = "http://blog.naver.com/PostView.nhn?blogId=" + id + "&logNo=" + no
+}
+
+if ("blog.naver.com" == location.host) {
+	var r = new RegExp("^(http|https)://(.*)/(.*)/(.*)"),
+		href = location.href;
+	if (r.test(href)) {
+		chkRun = true;
+		var exec_r = r.exec(href),
+			blog_id = exec_r[3],
+			blog_logNo = exec_r[4];
+		goBlog(blog_id, blog_logNo);
+	}
+} else {
+	var r = new RegExp(/^(http|https):\/\/(.+)\.blog\.me\/(.+)/),
+		href = location.href;
+	if (r.test(href)) {
+		chkRun = true;
+		var exec_r = r.exec(href),
+			blog_id = exec_r[1],
+			blog_logNo = exec_r[2];
+		goBlog(blog_id, blog_logNo);
+	}
+}
+
+if (!chkRun) {
+	alert("개발자에게 문의하세요")
+};
