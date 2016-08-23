79322c6c7314f7ce72f67b4de35f67721fbdb110
mooki <mooki83@gmail.com>
2016-08-23 10:18:25 +0900

    http://blog.naver.com/blogId/logNo? 형태일 경우 추가

--- book/naverblog_for_evernote.js
+++ book/naverblog_for_evernote.js
@@ -8,6 +8,7 @@ if ("blog.naver.com" == location.host) {
 	var r = new RegExp("^(http|https)://(.*)/(.*)/(.*)"),
 		href = location.href;
 	if (r.test(href)) {
+		console.log("type1");
 		chkRun = true;
 		var exec_r = r.exec(href);
 		goBlog(exec_r[3], exec_r[4]);
@@ -16,18 +17,29 @@ if ("blog.naver.com" == location.host) {
 	var r = new RegExp(/^(http|https):\/\/(.+)\.blog\.me\/(.+)/),
 		href = location.href;
 	if (r.test(href)) {
+		console.log("type2");
 		chkRun = true;
 		var exec_r = r.exec(href);
-		goBlog(exec_r[1], exec_r[2]);
+		goBlog(exec_r[2], exec_r[3]);
 	} else {
 		var el = document.getElementById("screenFrame");
 		if (el != null) {
 			var r = new RegExp(/blog.naver.com\/(.+)\?(.+)logNo=(.+)\&/),
 				src = el.src;
 			if (r.test(src)) {
+				console.log("type3");
 				chkRun = true;
 				var exec_r = r.exec(src);
 				goBlog(exec_r[1], exec_r[3]);
+			} else {
+				// SRC가 http://blog.naver.com/blogId/logNo? 형태일 경우
+				var r = new RegExp(/^(http|https):\/\/blog\.naver\.com\/(.*)\/(.\d+)/);
+				if ( r.test(src) ) {
+					console.log("type4");
+					chkRun = true;
+					var exec_r = r.exec(src);
+					goBlog(exec_r[1], exec_r[3]);
+				}
 			}
 		}
 	}
