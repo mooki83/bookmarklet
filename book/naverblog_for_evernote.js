b92688994b72eb1ea2bf287fc877dabf71e93b2a
mooki <mooki83@gmail.com>
2016-08-23 10:31:09 +0900

    주석 추가

--- book/naverblog_for_evernote.js
+++ book/naverblog_for_evernote.js
@@ -5,6 +5,7 @@ function goBlog(blog_id, blog_logNo) {
 }
 
 if ("blog.naver.com" == location.host) {
+	// 블로그 주소로 되어있을때 처리
 	var r = new RegExp("^(http|https)://(.*)/(.*)/(.*)"),
 		href = location.href;
 	if (r.test(href)) {
@@ -14,6 +15,7 @@ if ("blog.naver.com" == location.host) {
 		goBlog(exec_r[3], exec_r[4]);
 	}
 } else {
+	// blog.me 도메인일 경우 처리
 	var r = new RegExp(/^(http|https):\/\/(.+)\.blog\.me\/(.+)/),
 		href = location.href;
 	if (r.test(href)) {
@@ -22,8 +24,10 @@ if ("blog.naver.com" == location.host) {
 		var exec_r = r.exec(href);
 		goBlog(exec_r[2], exec_r[3]);
 	} else {
+		// 도메인 형태로 되어있을 경우 처리
 		var el = document.getElementById("screenFrame");
 		if (el != null) {
+			// SRC가 http://blog.naver.com/블로그아이디?Redirect=Log&logNo=글번호&........... 형태일 경우
 			var r = new RegExp(/blog.naver.com\/(.+)\?(.+)logNo=(.+)\&/),
 				src = el.src;
 			if (r.test(src)) {
@@ -32,7 +36,7 @@ if ("blog.naver.com" == location.host) {
 				var exec_r = r.exec(src);
 				goBlog(exec_r[1], exec_r[3]);
 			} else {
-				// SRC가 http://blog.naver.com/blogId/logNo? 형태일 경우
+				// SRC가 http://blog.naver.com/블로그아이디/글번호? 형태일 경우
 				var r = new RegExp(/^(http|https):\/\/blog\.naver\.com\/(.*)\/(.\d+)/);
 				if ( r.test(src) ) {
 					console.log("type4");
